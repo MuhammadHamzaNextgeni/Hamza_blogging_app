c776fd41ca23bb67029ce183d6ed4aaf7014341b
MuhammadHamzaNextgeni <hamza.mushtaque@nextgeni.com>
2025-09-30 17:50:50 +0500

    popup implementation

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostDetailView, PostListAPI, PostDetailAPI, PostCreateAPI, PostUpdateView, PostUpdateAPI,PostDeleteView, PostDeleteAPI, PostCommentCreateAPI,PostToggleLikeAPI,AddCommentView,ToggleLikeView
+from .views import PostListView, PostCreateView, PostDetailView, PostListAPI, PostDetailAPI, PostCreateAPI, PostUpdateView, PostUpdateAPI,PostDeleteView, PostDeleteAPI, PostCommentCreateAPI,PostToggleLikeAPI,AddCommentView,ToggleLikeView,AuthorSuggestionsView,AuthorSuggestionsAPI
 
 app_name = "posts"
 
@@ -11,9 +11,9 @@ urlpatterns = [
     path("<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path('post/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
     path('post/<int:pk>/like/', ToggleLikeView.as_view(), name='toggle_like'),
+    path("author-suggestions/", AuthorSuggestionsView.as_view(), name="author-suggestions"),
 
-
-    # endpoints for json 
+ # endpoints for json 
 
     path("api/", PostListAPI.as_view(), name="api-post-list"),
     path("api/new/", PostCreateAPI.as_view(), name="api-post-create"),
@@ -22,6 +22,8 @@ urlpatterns = [
     path("api/<int:pk>/delete/", PostDeleteAPI.as_view(), name="api-post-delete"),
     path('api/<int:post_pk>/comments/', PostCommentCreateAPI.as_view(), name='api-comment-create'),
     path('api/<int:post_pk>/like/', PostToggleLikeAPI.as_view(), name='api-toggle-like'),
+    path("api/author-suggestions/", AuthorSuggestionsAPI.as_view(), name="author-suggestions-api"),
+
 
 ]
 
