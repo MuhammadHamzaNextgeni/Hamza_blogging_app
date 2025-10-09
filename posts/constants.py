5b38b917d4aabdf19d0c709fcea942bbc297cd52
MuhammadHamzaNextgeni <hamza.mushtaque@nextgeni.com>
2025-10-09 18:22:32 +0500

    generic messages

---
+++ posts/constants.py
@@ -0,0 +1,3 @@
+UPDATE_POST_NOT_ALLOWED_MESSAGE = "You cannot update someone else's post."
+DELETE_POST_NOT_ALLOWED_MESSAGE = "You cannot delete someone else's post."
+POST_DELETE_SUCCESS_MESSAGE = "Post deleted successfully."
